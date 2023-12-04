e60d4053a464c0dbd1c9e33dde7e66d8f05b85df
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-12-04 09:31:36 -0800

    create initial modal

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,5 +1,27 @@
+import { InitialModal } from "@/components/modals/initial-modal";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
 const SetupPage = async  () => {
-    return (<div>create a server</div>);
+
+    //get the user profile
+    const profile = await initialProfile();
+    //check the profile in any server
+    const server = await db.server.findFirst({
+        where:{
+            members:{
+                some:{
+                    profileId: profile.id
+                }
+            }
+        }
+    })
+
+    if(server){
+        return redirect(`/servers/${server.id}`)
+    }
+    return (<InitialModal />);
 }
  
 export default SetupPage; 
