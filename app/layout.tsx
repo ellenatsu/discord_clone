c7296747db3366438e072fc575d4a7a34a114d5c
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-12-13 09:37:54 -0800

    add chat message

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -35,8 +36,10 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <SocketProvider>
-            <ModalProvider />
-            {children}
+              <ModalProvider />
+              <QueryProvider>
+                {children}
+              </QueryProvider>
             </SocketProvider>
           
           </ThemeProvider>
